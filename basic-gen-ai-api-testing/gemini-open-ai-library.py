e84fbbd9dca605c0af2239a99655883cc524fa58
JAYA DUBEY <jayadubey6402@gmail.com>
2025-10-23 00:45:47 +0530

    Added requirements.txt and two gemini api client input-output programs

---
+++ basic-gen-ai-api-testing/gemini-open-ai-library.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+client = OpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain me Open AI compatibility Gemini program"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
